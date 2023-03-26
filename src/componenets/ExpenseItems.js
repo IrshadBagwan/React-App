6172baa2c2cad7f88dcbddab472b73489762cfa1
IrshadBagwan <irshadbagwan706@gmail.com>
2023-03-26 12:35:38 +0530

    ExpenseItem

---
+++ src/componenets/ExpenseItems.js
@@ -0,0 +1,19 @@
+function ExpenseItem() {
+  return(
+  <div>
+      <h1>Expense Items</h1>
+    <div>Food Rs 10</div>
+    <div>
+      Petrol Rs 100
+    </div>
+    <div>Movies Rs 200</div>
+  </div>
+  );
+}
+export default ExpenseItem;
+
+// Food Rs 10
+
+// Petrol Rs 100
+
+// Movies Rs 200
