8c03122da5a7cdd2a5c45dfaf73bb8fefae9fefe
IrshadBagwan <irshadbagwan706@gmail.com>
2023-04-01 14:22:36 +0530

    changeamount function

--- src/componenets/Expenses/ExpenseDetails.js
+++ src/componenets/Expenses/ExpenseDetails.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 import ExpenseDate from "./ExpenseDate.js";
+import Expenses from './Expenses';
 const ExpenseDetail=(props)=>{
+
+  const [title,setTitle] = useState(props.title);
   
+  const [amount,setAmount] = useState(props.amount);
+  function changeExpenses(){
+     setAmount("$100");
+
+  }
+
+  function clickhandler(){
+    
+  setTitle('updated!');
+   console.log(title);
+  //   return(
+  // props.itm.map((el,i)=>{
+       
+    
+  //     if(el[i].id===id){
+  //       props.itm.splice(i,1);
+  //   }
+  //   }))
+  }
     return(
      
         <div className="expense-item">
        <ExpenseDate date={props.date}/>
        
           <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            
+            
+            <h2>{title}</h2>
           </div>
           <div className="expense-item__description">{props.location}</div>
-          <div className="expense-item__price">{props.amount}</div>
+          <div className="expense-item__price">{amount}</div>
+          <button onClick={clickhandler}>Delete Task</button>
+          <button onClick={changeExpenses}>change Amount</button>
         </div>
+        
         
         
     );
