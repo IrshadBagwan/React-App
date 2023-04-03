fae8fb0b1f0ebf1f329d3790efe1a5a3a1d00f52
IrshadBagwan <irshadbagwan706@gmail.com>
2023-04-03 11:59:33 +0530

    ExpenseFilter

--- src/componenets/Expenses/Expenses.js
+++ src/componenets/Expenses/Expenses.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ExpenseFilter from './ExpenseFilter.js';
 import ExpenseItem from "./ExpenseItems.js";
 import "./Expenses.css";
 
 
+
 const Expenses=(props)=>{
+
+  const[filteredYear,setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear =>{
+    setFilteredYear(selectedYear);
+  }
     return(
        <div className="expenses">
-           
+           <ExpenseFilter selected = {filteredYear} onChangeFilter={filterChangeHandler}></ExpenseFilter>
               {props.items.map((exp)=>{
                 
                 return(
-                  <ExpenseItem title={exp.title} locexp={exp.locationofexpendeture} amount={exp.amount} date={exp.date}></ExpenseItem>
+                  <ExpenseItem ite={props.items} title={exp.title} locexp={exp.locationofexpendeture} amount={exp.amount} date={exp.date} id={exp.id} ></ExpenseItem>
                 )
        
                })} 
